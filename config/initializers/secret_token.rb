6b1fd687ecca4d4c6184b2a5ebca568d83e8d18b
Drew Butler <abutler3@gmail.com>
2013-10-26 10:00:09 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoTracker::Application.config.secret_token = 'eca78d56213a8bf2ab562423ce160f0bcc9949105af839df920b6e685e3f81455a354da0468482dde57d12d7d342836458fa653af5ad7f6c2277c70b153bf431'
