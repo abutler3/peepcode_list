78d081b6522cb00d3a88b18cbfa412a353ed2c0b
Drew Butler <abutler3@gmail.com>
2013-10-27 18:26:13 -0500

    Add else if no video can be gotten from database

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -11,4 +11,18 @@ class VideosController < ApplicationController
     @video = Video.find(params[:id])
     respond_with @video
   end
+
+  def update
+    @video = Video.find(params[:id])
+    if [true, false, "true", "false"].include?(params[:watched])
+    # If params are true or false, then update
+      @video.update_attributes(watched: params[:watched])
+      # Call update_attributes and only updated the watched attributed and
+      # expect a parameter passed in called watched
+      # Sets value of watched and save
+      respond_with @video
+    else
+      render nothing: true, status: 304 #not modified
+    end
+  end
 end
